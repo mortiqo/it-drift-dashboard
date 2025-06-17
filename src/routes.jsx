68283fa47f96dcfbbab0e9542996653019a1411b
mortiqo <mortenollestad451@gmail.com>
2025-06-17 11:39:21 +0200

    test 2

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './components/Login';
+import Login from './components/login';
 import Dashboard from './components/dashboard';
 import NotFound from './components/notfound';
 import Admin from './components/Admin';
